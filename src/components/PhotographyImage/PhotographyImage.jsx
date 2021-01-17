901d516e4cd44a88048204aa55a2b19bbb7dc39c
Dannah Gellina Simbulan <dgellina@gmail.com>
2021-01-17 17:05:17 -0500

    added photography images, dummy photo data, and added them to photography archive and home page

---
+++ src/components/PhotographyImage/PhotographyImage.jsx
@@ -0,0 +1,37 @@
+import { Image } from "semantic-ui-react";
+
+import "./PhotographyImage.scss";
+
+function PhotographyImage(props) {
+  const monthNames = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+  const date = new Date(props.photoData.date);
+  const month = monthNames[date.getUTCMonth()];
+  const year = date.getUTCFullYear();
+  const newDate = month + ` ` + year.toString();
+  return (
+    <div className="single-image-container">
+      <Image src={props.photoData.src} size={props.size} />
+      <div className="image-overlay">
+        <div className="image-text">
+          <div className="image-location">{props.photoData.location}</div>
+          <div className="image-date">{newDate}</div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default PhotographyImage;
