c72eafc0ddf7d39c32716df207086314f38a6ee2
Dannah Gellina Simbulan <dgellina@gmail.com>
2021-06-15 19:38:38 -0400

    Update about us page and contact information

--- src/About/About.jsx
+++ src/About/About.jsx
@@ -1,13 +1,67 @@
 import Navbar from "../components/Navbar/Navbar.jsx";
 import Footer from "../components/Footer/Footer.jsx";
 
+import { Container, Icon, Image } from "semantic-ui-react";
+
 import "./About.scss";
 
 function About(props) {
+  const onInstagramClick = () => {
+    window.open("https://www.instagram.com/advntr.archive/", "_blank");
+  };
+
   return (
     <div className="about-container">
       <Navbar {...props} />
-      <div className="about-content">About</div>
+      <div className="about-content">
+        <Image
+          className="about-us-image"
+          src="https://adventures-archive.s3.amazonaws.com/32FC7ECA-FB34-4A83-BC8D-15E6FE9CBB31.JPG"
+          size="large"
+        />
+        <div className="about-text">
+          <div className="about-header">Our Story</div>
+          <Container text fluid className="our-story-container">
+            <p>
+              We share a love for food, photography, and exploring new spaces.
+              We took our first trip together in 2017 and have been exploring
+              together ever since. We believe adventures can be found anywhere,
+              from neighborhoods close to home or cities across the globe. After
+              a few years of traveling together, we decided to create this space
+              to archive our memories and adventures.
+            </p>
+            <p>
+              A few of our shared hobbies include: bouldering and rock climbing,
+              shooting photography with our film and digital cameras, and
+              finding delicious places to eat. If you happen to find yourself on
+              our site, we hope our adventures inspire you to go out on your own
+              adventures and create cherished memories.
+            </p>
+          </Container>
+          <Container
+            text
+            textAlign="center"
+            fluid
+            className="signature-container"
+          >
+            <p>~ Dannah and Denny</p>
+          </Container>
+
+          <div className="contact-us-info">
+            <div className="contact-us-heading">Contact Us</div>
+            <div
+              className="instagram-handle"
+              onClick={() => onInstagramClick()}
+            >
+              <Icon name="instagram" size="large" />
+              advntr.archive
+            </div>
+            <a className="email" href="mailto:hello@advntrarchive.com">
+              <Icon name="mail outline" size="large" /> hello@advntrarchive.com
+            </a>
+          </div>
+        </div>
+      </div>
       <Footer />
     </div>
   );
--- src/Home/components/HeroHeader.jsx
+++ src/Home/components/HeroHeader.jsx
@@ -3,7 +3,7 @@ import "./HeroHeader.scss";
 function HeroHeader(props) {
   return (
     <div className="hero-header-container">
-      <div className="hero-header">Dannah X Denny</div>
+      <div className="hero-header">Dannah & Denny</div>
       <div className="header-tag">sharing our adventures with you</div>
     </div>
   );
