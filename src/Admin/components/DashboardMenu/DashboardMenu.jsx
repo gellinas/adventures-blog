ec38d9c21a089e07fe5e7a352a337968dab06786
Dannah Gellina Simbulan <dgellina@gmail.com>
2021-09-11 17:54:02 -0400

    Add prompt when switching forms

--- src/Admin/components/DashboardMenu/DashboardMenu.jsx
+++ src/Admin/components/DashboardMenu/DashboardMenu.jsx
@@ -1,10 +1,21 @@
-import { Menu, Divider } from "semantic-ui-react";
+import { useState } from "react";
+
+import { Menu, Divider, Modal, Button, Icon, Header } from "semantic-ui-react";
 
 import "./DashboardMenu.scss";
 
 function DashboardMenu(props) {
-  const handleMenuItemClick = (event, { name }) => {
-    props.setActiveMenuItem(name);
+  const [openFormChangeModal, setOpenFormChangeModal] = useState(false);
+  const [storedActiveMenuItem, setStoredActiveMenuItem] = useState(null);
+
+  const handleMenuModalClick = (event, { name }) => {
+    setStoredActiveMenuItem(name);
+    setOpenFormChangeModal(true);
+  };
+
+  const handleMenuItemClick = () => {
+    props.setActiveMenuItem(storedActiveMenuItem);
+    setOpenFormChangeModal(false);
   };
 
   return (
@@ -13,7 +24,7 @@ function DashboardMenu(props) {
         <Menu.Item
           name="dashboard"
           active={props.activeMenuItem === "dashboard"}
-          onClick={handleMenuItemClick}
+          onClick={handleMenuModalClick}
         />
 
         <Divider />
@@ -23,12 +34,12 @@ function DashboardMenu(props) {
           <Menu.Item
             name="create blog post"
             active={props.activeMenuItem === "create blog post"}
-            onClick={handleMenuItemClick}
+            onClick={handleMenuModalClick}
           />
           <Menu.Item
             name="edit published post"
             active={props.activeMenuItem === "edit published post"}
-            onClick={handleMenuItemClick}
+            onClick={handleMenuModalClick}
           />
         </Menu.Menu>
 
@@ -39,15 +50,36 @@ function DashboardMenu(props) {
           <Menu.Item
             name="add photography"
             active={props.activeMenuItem === "add photography"}
-            onClick={handleMenuItemClick}
+            onClick={handleMenuModalClick}
           />
           <Menu.Item
             name="edit photography"
             active={props.activeMenuItem === "edit photography"}
-            onClick={handleMenuItemClick}
+            onClick={handleMenuModalClick}
           />
         </Menu.Menu>
       </Menu>
+
+      <Modal
+        onClose={() => setOpenFormChangeModal(false)}
+        onOpen={() => setOpenFormChangeModal(true)}
+        open={openFormChangeModal}
+        size="small"
+      >
+        <Modal.Header>Would you like to exit this form?</Modal.Header>
+        <Modal.Actions>
+          <Button
+            basic
+            color="red"
+            onClick={() => setOpenFormChangeModal(false)}
+          >
+            <Icon name="remove" /> No
+          </Button>
+          <Button color="green" inverted onClick={handleMenuItemClick}>
+            <Icon name="checkmark" /> Yes
+          </Button>
+        </Modal.Actions>
+      </Modal>
     </div>
   );
 }
